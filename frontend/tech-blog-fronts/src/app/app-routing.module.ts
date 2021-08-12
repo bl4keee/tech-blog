dfe35e4b3a11660b3aff4aa33df3661e82e63fef
Bartek <bl4kee@wp.pl>
2021-08-12 10:20:31 +0200

    Added new route for 'upload-video'.

--- frontend/tech-blog-fronts/src/app/app-routing.module.ts
+++ frontend/tech-blog-fronts/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {UploadVideoComponent} from "./upload-video/upload-video.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'upload-video', component: UploadVideoComponent,
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
